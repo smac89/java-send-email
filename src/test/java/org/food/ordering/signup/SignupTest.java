b053ab649cf29ed925d2388908ebced599bf36a0
smac89 <smac89@users.noreply.github.com>
2017-12-05 22:07:45 -0600

    Added fix for asynchronous test

--- src/test/java/org/food/ordering/signup/SignupTest.java
+++ src/test/java/org/food/ordering/signup/SignupTest.java
@@ -2,6 +2,9 @@ package org.food.ordering.signup;
 
 import org.junit.Test;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -14,16 +17,20 @@ public class SignupTest {
         assertNotNull(confirmationCode);
     }
 
-    @Test
-    public void confirmationCodeWithCallback_willCallTheCallbackWithConfirmationCode() {
-        SignUp signUp = new SignUp("myusername@myhost.com");
-        final boolean[] callbackCalled = {false};
-        signUp.sendConfirmationEmail(new EmailCallback() {
-            @Override
-            public void emailSent(String confirmationCode) {
-                callbackCalled[0] = true;
-            }
-        });
-        assertTrue(callbackCalled[0]);
-    }
+@Test
+public void confirmationCodeWithCallback_willCallTheCallbackWithConfirmationCode() throws InterruptedException {
+    final CountDownLatch lock = new CountDownLatch(1);
+    SignUp signUp = new SignUp("myusername@myhost.com");
+    final boolean[] callbackCalled = {false};
+    signUp.sendConfirmationEmail(new EmailCallback() {
+        @Override
+        public void emailSent(String confirmationCode) {
+            callbackCalled[0] = true;
+            lock.countDown();
+        }
+    });
+
+    lock.await(10, TimeUnit.SECONDS);
+    assertTrue(callbackCalled[0]);
+}
 }
